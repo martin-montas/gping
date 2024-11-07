fe3b90b0f2d372748a83abb96b29b38f95e913e6
martin-montas <martinmontas1@gmail.com>
2024-11-06 21:07:38 -0500

    Feat: added support for IPv6 support

--- ICMPRequest/cidr-support.go
+++ ICMPRequest/cidr-support.go
@@ -7,6 +7,21 @@ import (
 	"strconv"
 )
 func isCidrVAlidIpv6(ip string) bool {
+	// /0 to /128 are valid ranges.
+	lastTwo := ip[len(ip)-2:]
+	if !strings.ContainsRune(ip,'/') {
+		return false
+	}
+	if strings.Index(string(lastTwo[0]), "/") != 0 {
+		num, err := strconv.Atoi(lastTwo)
+		if err != nil {
+			return false
+		}
+		if num > 128 {
+			return false
+		}
+		return true
+	}
 	return false
 }
 
@@ -30,7 +45,15 @@ func isCidrVAlidIpv4(ip string) bool {
 }
 
 func handleCidr6(ip string) { 
-	return
+	_, ipNet, err := net.ParseCIDR(ip)
+	if err != nil {
+		fmt.Println("Error parsing CIDR:", err)
+		return
+	}
+	firstIP := ipNet.IP
+	for ip := firstIP.Mask(ipNet.Mask); ipNet.Contains(ip); incrementIP(ip) {
+		sendIP(ip.String())
+	}
 }
 func handleCidr4(ip string) { 
 	_, ipNet, err := net.ParseCIDR(ip)
