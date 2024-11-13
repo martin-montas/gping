93860b730c0c92ad22b9d78fcaeebbca36b0dbd9
martin-montas <martinmontas1@gmail.com>
2024-11-13 15:55:24 -0500

    Got support for CIDR for IPv4 and: Need to fix a bug now

--- ICMPRequest/icmp-packet.go
+++ ICMPRequest/icmp-packet.go
@@ -1,26 +1,40 @@
 package ICMPRequest
 
 import (
-	"fmt"
-	"strings"
-	"strconv"
-	"log"
-	"syscall"
+    "encoding/binary"
+    "syscall"
+    "strings"
+    "fmt"
+    // "net"
+    "os"
+    // "syscall"
 )
 
-func checksum(data []byte) uint16 {
-	var sum uint32
-	for i := 0; i < len(data)-1; i += 2 {
-		sum += uint32(data[i])<<8 + uint32(data[i+1])
-	}
-	if len(data)%2 != 0 {
-		sum += uint32(data[len(data)-1]) << 8
-	}
-	for (sum >> 16) > 0 {
-		sum = (sum & 0xFFFF) + (sum >> 16)
-	}
-	return ^uint16(sum)
+
+// ICMP packet structure
+type icmpPacket struct {
+    Type     uint8
+    Code     uint8
+    Checksum uint16
+    ID       uint16
+    Sequence uint16
+    Data     []byte
 }
+
+
+func calculateChecksum(data []byte) uint16 {
+    var sum uint32
+    for i := 0; i < len(data)-1; i += 2 {
+        sum += uint32(binary.BigEndian.Uint16(data[i:]))
+    }
+    if len(data)%2 == 1 {
+        sum += uint32(data[len(data)-1]) << 8
+    }
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    return uint16(^sum)
+}
+
 
 func RunProgram(ip string) {
 	isValidisCidr4 := isCidrVAlidIpv4(ip)
@@ -31,22 +45,10 @@ func RunProgram(ip string) {
 	if isValidisCidr6 {
 		handleCidr6(ip)
 	} else {
-		sendPacket(ip)
+		// sendPacket(ip)
 	}
 }
 
-func handlePacket() []byte {
-	var packet = []byte{
-		8, 0, 0, 0, 
-		0, 1,
-		0, 1,
-		72, 101, 108, 108, 111, 
-	}
-	cs := checksum(packet)
-	packet[2] = byte(cs >> 8)   
-	packet[3] = byte(cs & 0xFF) 
-	return packet
-}
 
 func stringToByte(ip string) [4]byte {
 	strResult := strings.Split(ip, ".")
@@ -61,24 +63,30 @@ func stringToByte(ip string) [4]byte {
 	return ipByte
 }
 
-func createRawSocket(ip string) (int ,[]byte, syscall.SockaddrInet4) { 
-	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_ICMP)
-	if err != nil {
-		log.Fatalf("Failed to create raw socket: %v", err)
-	}
-	addr := syscall.SockaddrInet4{
-		Addr: stringToByte(ip), 
-	}
-	packet := handlePacket()
-	return sock, packet, addr
-}
 
-func sendPacket(ip string) {
-	sock, packet, addr := createRawSocket(ip)
-	defer syscall.Close(sock)
-	err := syscall.Sendto(sock, packet, 0, &addr)
-	if err != nil {
-		log.Fatalf("Failed to send packet: %v", err)
-	}
-		listenForICMP(sock)
+func sendPacket() {
+    packet := icmpPacket {
+        Type:     8, // Echo request
+        Code:     0,
+        Checksum: 0,
+        ID:       uint16(os.Getpid() & 0xffff),
+        Sequence: 1,
+        Data:     []byte("HELLO-ping"),
+    }
+    packetBytes := make([]byte, 8+len(packet.Data))
+    packetBytes[0] = packet.Type
+    packetBytes[1] = packet.Code
+    binary.BigEndian.PutUint16(packetBytes[2:4], packet.Checksum)
+    binary.BigEndian.PutUint16(packetBytes[4:6], packet.ID)
+    binary.BigEndian.PutUint16(packetBytes[6:8], packet.Sequence)
+    copy(packetBytes[8:], packet.Data)
+
+    // Calculate and set checksum
+    packet.Checksum = calculateChecksum(packetBytes)
+    binary.BigEndian.PutUint16(packetBytes[2:4], packet.Checksum)
+
+    // Resolve destination address
+    dst := &syscall.SockaddrInet4{
+        Port: 0,
+    }
 }
