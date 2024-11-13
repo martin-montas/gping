6102161644a7b60fc74e5c8905d3cf38b81c236e
martin-montas <martinmontas1@gmail.com>
2024-11-12 20:26:05 -0500

    Finish Support for Single IP Packets: Next Cidr Notation

--- ICMPRequest/listen-icmp.go
+++ ICMPRequest/listen-icmp.go
@@ -10,25 +10,29 @@ import (
 
 func listenForICMP(sockfd int) {
 	var buffer [2048]byte
-	n, from, err := syscall.Recvfrom(sockfd, buffer[:], 0)
+	n, _, err := syscall.Recvfrom(sockfd, buffer[:], 0)
 	if err != nil {
 		log.Fatal("Error receiving packet:", err)
 	}
+	interpretPacket(buffer[:n], n)
 	fmt.Printf("Captured %d bytes: %x\n", n, buffer[:n])
-	interpretPacket(buffer[:n], n, from)
 }
 
-func interpretPacket(buffer []byte, n int, from syscall.Sockaddr) {
-	msg, err := icmp.ParseMessage(ipv4.ICMPTypeEcho.Protocol(), buffer[:n])
-	if err != nil {
-		log.Printf("Error parsing ICMP message: %v", err)
-	}
-	if msg.Type == ipv4.ICMPTypeEcho {
-		fmt.Printf("Received ICMP Echo Request from %v\n", from)
-		if echo, ok := msg.Body.(*icmp.Echo); ok {
-			fmt.Printf("ID: %d, Seq: %d, Data: %s\n", echo.ID, echo.Seq, string(echo.Data))
-		}
-	} else {
-		fmt.Printf("Received non-echo ICMP message of type %v\n", msg.Type)
-	}
+func interpretPacket(buffer []byte, n int) {
+    msg, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), buffer[:n])
+    if err != nil {
+        log.Printf("Error parsing ICMP message: %v", err)
+        return
+    }
+    fmt.Printf("Packet Type: %d\n", msg.Type)
+    fmt.Printf("Packet Code: %d\n", msg.Code)
+    fmt.Printf("Packet Checksum: %d\n", msg.Checksum)
+
+    if msg.Type == ipv4.ICMPTypeEchoReply {
+        if echo, ok := msg.Body.(*icmp.Echo); ok {
+            fmt.Printf("Echo Data: %s\n", string(echo.Data))
+        } else {
+            log.Println("Error: Unexpected body type")
+        }
+    }
 }
