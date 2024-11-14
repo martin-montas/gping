2098d38a22a50beb5a2862d7011c4be4e9eefeca
martin-montas <martinmontas1@gmail.com>
2024-11-14 14:33:30 -0500

    Added a README.md file

--- ICMPRequest/listen-icmp.go
+++ ICMPRequest/listen-icmp.go
@@ -5,14 +5,20 @@ import (
 	"encoding/binary"
 )
 
-func RenderPacket(icmpReply []byte) {
+func RenderPacket(icmpReply []byte,ip string) {
 	if icmpReply[0] == 0 { // ICMP Echo Reply
+		fmt.Printf("IP: %s\n", ip)
 		fmt.Printf("Got echo reply!\n")
 		fmt.Printf("Type: %d\n", icmpReply[0])
 		fmt.Printf("Code: %d\n", icmpReply[1])
 		fmt.Printf("ID: %d\n", binary.BigEndian.Uint16(icmpReply[4:6]))
 		fmt.Printf("Sequence: %d\n", binary.BigEndian.Uint16(icmpReply[6:8]))
 		fmt.Printf("Data: %s\n", string(icmpReply[8:]))
+	} else {
+		fmt.Printf("IP: %s\n", ip)
+		fmt.Printf("This is an Error: \n")
+		fmt.Printf("Type: %d\n", icmpReply[0])
+		fmt.Printf("Code: %d\n", icmpReply[1])
 	}
 }
 
